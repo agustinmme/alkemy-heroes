a3ecb327072fe0b1f4d64cf1c9d3a0bc412f3185
agustinmme <76716674+agustinmme@users.noreply.github.com>
2021-10-13 14:42:12 -0300

    Add:services login and localStorage

--- src/views/pages/Login.jsx
+++ src/views/pages/Login.jsx
@@ -1,111 +1,143 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import logo from "../../assets/logo-alkemy.png";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-
+import loginServices from "../../services/login";
 function App() {
+  const [user, setUser] = useState({});
+  const [erroAlert, setErroAlert] = useState("");
   return (
     <>
       <div className="bg">
         <div className="d-flex justify-content-center container">
-          <div className="card my-auto">
-            <img
-              src={logo}
-              className="img-logo rounded mx-auto d-block mt-3 "
-              alt="Logo Alkemy"
-            />
-            <div className="px-3">
-              <Formik
-                initialValues={{
-                  email: "",
-                  password: "",
-                }}
-                validate={(values) => {
-                  let errors = {};
-                  if (!values.email) {
-                    errors.email = "ingresa un email";
-                  } else if (!values.email) {
-                    errors.email = "Ingrese un mail valido";
-                  }
+          <div className="card my-auto ">
+          {erroAlert!==""?<div className={"card-header"}>
+              <p className="text-center text-danger fw-bolder my-auto">
+                {erroAlert}
+              </p>
+            </div>:null}
+            <div className="card-body">
+              <img
+                src={logo}
+                className="img-logo rounded mx-auto d-block mt-3 "
+                alt="Logo Alkemy"
+              />
+              <div className="px-3">
+                <div id="emailHelp" className="text-center fs-5">
+                  Alkemy Heroes
+                </div>
+                <Formik
+                  initialValues={{
+                    email: "",
+                    password: "",
+                  }}
+                  validate={(values) => {
+                    let errors = {};
+                    if (!values.email) {
+                      errors.email = "Debes ingresar tu email/usuario";
+                    } else if (
+                      !/^[a-z0-9_.]+@[a-z0-9]+\.[a-z0-9_.]+$/i.test(
+                        values.email
+                      )
+                    ) {
+                      errors.email = "El formato no es valido";
+                    }
 
-                  if (!values.password) {
-                    errors.password = "ingresa un password";
-                  } else if (!values.password) {
-                    errors.password = "ingresa una contresena valida";
-                  }
-                  return errors;
-                }}
-                onSubmit={(values, { resetForm }) => {
-                  console.log("hola" + values.email);
-                  resetForm();
-                }}
-              >
-                {({
-                  errors
-                }) => (
-                  <Form className="form">
-                    <div className="d-flex justify-content-center align-items-center">
-                      <div id="emailHelp" className="form-text fs-5">
-                        Alkemy Heroes
+                    if (!values.password) {
+                      errors.password = "Debes ingresar tu contraseña";
+                    }
+                    return errors;
+                  }}
+                  onSubmit={async (values, { resetForm }) => {
+                    try {
+                      const user = await loginServices.login({
+                        email: values.email,
+                        password: values.password,
+                      });
+                      console.log("hola" + values.email);
+                      setUser(user);
+                      resetForm();
+                      const { token } = user;
+                      window.localStorage.setItem("LoggedAlkemyChallenge",JSON.stringify(token))
+                    } catch (error) {
+                        setErroAlert(error.message);
+                        setTimeout(()=>{
+                            setErroAlert("");
+                        },4000);
+                    }
+                  }}
+                >
+                  {({ errors, touched }) => (
+                    <Form className="form">
+                      <div className="d-flex justify-content-center align-items-center"></div>
+                      <div className="d-flex justify-content-center align-items-center"></div>
+                      <div className="mb-3">
+                        <label className="form-label mt-3">Email address</label>
+                        <Field
+                          type="email"
+                          className={
+                            errors.email && touched.email
+                              ? "form-control is-invalid"
+                              : "form-control"
+                          }
+                          id="email"
+                          name="email"
+                          placeholder="Agustin@gmail.com"
+                        />
+                        <ErrorMessage
+                          name="email"
+                          component={() => (
+                            <span
+                              id="emailError"
+                              className="text-danger fw-bolder mt-2"
+                            >
+                              {errors.email}
+                            </span>
+                          )}
+                        />
                       </div>
-                    </div>
-                    <div className="d-flex justify-content-center align-items-center"></div>
-                    <div className="mb-3">
-                      <label className="form-label mt-3">Email address</label>
-                      <Field
-                        type="email"
-                        className="form-control"
-                        id="email"
-                        name="email"
-                        placeholder="Correo electrónico"
-                      />
-                      <ErrorMessage
-                        name="email"
-                        component={() => (
-                          <div
-                            id="emailError"
-                            className="text-danger fw-bolder mt-2"
-                          >
-                            {errors.email}
-                          </div>
-                        )}
-                      />
-                    </div>
 
-                    <div className="mb-3">
-                      <label className="form-label ">Password</label>
-                      <Field
-                        type="password"
-                        className="form-control"
-                        id="password"
-                        name="password"
-                        placeholder="Contraseña"
-                      />
-                      <ErrorMessage
-                        name="password"
-                        component={() => (
-                          <div
-                            id="passwordError"
-                            className="text-danger fw-bolder mt-2"
-                          >
-                            {errors.password}
-                          </div>
-                        )}
-                      />
-                    </div>
+                      <div className="mb-3">
+                        <label className="form-label ">Password</label>
+                        <Field
+                          type="password"
+                          className={
+                            errors.password && touched.password
+                              ? "form-control is-invalid"
+                              : "form-control"
+                          }
+                          id="password"
+                          name="password"
+                          placeholder="Contraseña"
+                        />
+                        <ErrorMessage
+                          name="password"
+                          component={() => (
+                            <span
+                              id="passwordError"
+                              className="text-danger fw-bolder mt-2"
+                            >
+                              {errors.password}
+                            </span>
+                          )}
+                        />
+                      </div>
 
-                    <button
-                      type="submit"
-                      className="btn btn-primary fw-light font-monospace "
-                    >
-                      Login
-                    </button>
-                  </Form>
-                )}
-              </Formik>
+                      <button
+                        type="submit"
+                        className={"btn btn-primary fw-light font-monospace "}
+                      >
+                        Login
+                      </button>
+                    </Form>
+                  )}
+                </Formik>
+              </div>
             </div>
-            <div className="card-body text-light mx-auto mt-2">
-              Made with 💙 by Mansilla Agustin
+            <div className={"card-footer "}>
+              <p className="text-center my-auto">
+                Made with 💙 by Mansilla Agustin
+              </p>
             </div>
           </div>
         </div>
