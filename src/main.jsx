1e2a83dc5cf0ab3f7c83f0099fea67d64df622b6
agustinmme <76716674+agustinmme@users.noreply.github.com>
2021-10-14 13:49:15 -0300

    Add:Services login,Routers,assets

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './views/pages/Login'
+import App from './routes/AppRoutes'
 
 ReactDOM.render(
   <React.StrictMode>
