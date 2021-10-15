ee664c737d2be0896d821b31ce91e6abdee4b76c
agustinmme <76716674+agustinmme@users.noreply.github.com>
2021-10-15 08:06:08 -0300

    Revert "Add:Services login,Routers,assets"
    This reverts commit 1e2a83dc5cf0ab3f7c83f0099fea67d64df622b6.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './routes/AppRoutes'
+import App from './views/pages/Login'
 
 ReactDOM.render(
   <React.StrictMode>
