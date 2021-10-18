2940773b3ba2c23a8038bcce9939aaed4c1e0b72
agustinmme <76716674+agustinmme@users.noreply.github.com>
2021-10-18 13:54:48 -0300

    Revert "Feat/search-hero"

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import auth from './authReducers';
-import superheroes from './superheroReducers'
-import {combineReducers} from "redux";
-
-
-export default combineReducers({
-    auth,superheroes
-})
