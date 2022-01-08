a7e3c85ca23a40328a8b889fc6fddce6e171a97f
agustinmme <agustinmansilla240@gmail.com>
2022-01-08 17:16:53 -0300

    Delete loading spinner librery

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,24 +1,13 @@
 import React from "react";
-import { LoopCircleLoading } from "react-loadingg";
-import Logo from "../Logo/Logo";
+
 import PropTypes from "prop-types";
-const Loader = ({ type = 1 }) => {
+import "./Loader.css";
+const Loader = () => {
   return (
     <>
-      {type !== 1 ? (
-        <div>
-          <section className="d-flex text-center">
-            <div className="container d-flex justify-content-center mt-5">
-              <div className="row align-items-center justify-content-center mt-5">
-                <Logo />
-              </div>
-            </div>
-          </section>
-          <LoopCircleLoading />
-        </div>
-      ) : (
-        <LoopCircleLoading />
-      )}
+      <div className="spinner-border fixed" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
     </>
   );
 };
@@ -26,6 +15,5 @@ const Loader = ({ type = 1 }) => {
 Loader.propTypes = {
   type: PropTypes.number,
 };
-
 
 export default Loader;
